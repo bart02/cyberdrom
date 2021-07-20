9039a1deaea101bcd9e4ef6b27d0cbef725e4ac9
Artem Batalov <a@batalov.me>
2021-07-21 00:59:54 +0300

    Hotfix

--- 1_post/egor_vova.lua
+++ 1_post/egor_vova.lua
@@ -1,19 +1,18 @@
 -- https://learnxinyminutes.com/docs/ru-ru/lua-ru/ ссылка для быстрого ознакомления с основами языка LUA
 
-
 -- Создание порта управления магнитом - порт PC3 на плате версии 1.2
 local magnet = Gpio.new(Gpio.C, 3, Gpio.OUTPUT)
 local magnet_state = false
 
 -- Функция переключения магнита
-    local function toggleMagnet()
-        if (magnet_state == true) then  -- Если магнит включен, то     выключаем его
-             magnet:reset()
-        else                            -- Если выключен, то включаем
-            magnet:set()
-        end
-       magnet_state = not magnet_state -- Инвертируем переменную состояния
+local function toggleMagnet()
+    if (magnet_state == true) then  -- Если магнит включен, то     выключаем его
+            magnet:reset()
+    else                            -- Если выключен, то включаем
+        magnet:set()
     end
+    magnet_state = not magnet_state -- Инвертируем переменную состояния
+end
 -- количество светодиодов на основной плате пионера
 local ledNumber = 4
 -- создание порта управления светодиодами
@@ -34,13 +33,13 @@ end
 
 -- таблица цветов в формате RGB для передачи в функцию changeColor
 local colors = {
-        {1, 0, 0}, -- красный
-        {0, 1, 0}, -- зеленый
-        {1, 1, 0}, -- желтый
-        {1, 0, 1}, -- фиолетовый
-        {0, 0, 1}, -- синий
-        {1, 1, 1}, -- белый
-        {0, 0, 0}  -- черный/отключение светодиодов
+    {1, 0, 0}, -- красный
+    {0, 1, 0}, -- зеленый
+    {1, 1, 0}, -- желтый
+    {1, 0, 1}, -- фиолетовый
+    {0, 0, 1}, -- синий
+    {1, 1, 1}, -- белый
+    {0, 0, 0}  -- черный/отключение светодиодов
 }
 
 local points = {
@@ -54,26 +53,30 @@ local curColor = 1
 local function nextPoint()
     curColor = (curPoint-1)%(#colors-2)+1
     changeColor(colors[curColor])
-        if curPoint <= #points then
-            if #points[curPoint] == 3 then
-                Timer.callLater(2, function ()
-                    ap.goToLocalPoint(unpack(points[curPoint]))
-                    curPoint = curPoint + 1 
-                end)
-            end
-            if #points[curPoint] == 0 then
-                Timer.callLater(2, function ()
-                ap.push(Ev.MCE_LANDING) -- отправка команды автопилоту на посадку
+    if curPoint <= #points then
+        if #points[curPoint] == 3 then
+            Timer.callLater(2, function ()
+                ap.goToLocalPoint(unpack(points[curPoint]))
+                curPoint = curPoint + 1
             end)
-                toggleMagnet()
-
-        else
-            changeColor(colors[6]) -- смена цвета светодиодов на белый
+        end
+        if #points[curPoint] == 0 then
             Timer.callLater(2, function ()
                 ap.push(Ev.MCE_LANDING) -- отправка команды автопилоту на посадку
             end)
+            toggleMagnet()
+            Timer.callLater(2, function ()
+            ap.push(Ev.MCE_TAKEOFF)
+            end)
         end
 
+    else
+        changeColor(colors[6]) -- смена цвета светодиодов на белый
+        Timer.callLater(2, function ()
+            ap.push(Ev.MCE_LANDING) -- отправка команды автопилоту на посадку
+        end)
+    end
+end
 
 
 -- функция обработки событий, автоматически вызывается автопилотом
@@ -92,18 +95,13 @@ function callback(event)
     if (event == Ev.POINT_REACHED) then
         nextPoint()
     end
-    
+
     -- если пионер приземлился, то выключаем светодиоды
     if (event == Ev.COPTER_LANDED) then
         changeColor(colors[7])
     end
 
 end
-
-
-
-
-
 
 
 changeColor(colors[6]) -- смена цвета светодиодов на белый
